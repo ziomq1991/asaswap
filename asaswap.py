3e58324e6025c042b8f53af5fc15456351aedf6f
krystiansiwek <krystian.siwek@yahoo.com>
2020-11-17 16:21:31 +0100

    cosmetic changes, making contracts even simpler

--- asaswap.py
+++ asaswap.py
@@ -1,31 +1,35 @@
 from pyteal import *
+from state import GlobalState, LocalState
 
 
 def state(ratio_decimal_points):
+    # globals
+    TOTAL_LIQUIDITY_TOKENS = GlobalState('TOTAL_LIQUIDITY_TOKENS')
+    ALGOS_BALANCE = GlobalState('ALGOS_BALANCE')
+    TOKENS_BALANCE = GlobalState('TOKENS_BALANCE')
+    EXCHANGE_RATE = GlobalState('EXCHANGE_RATE')
+    ESCROW_ADDR = GlobalState('ESCROW_ADDR')
+    CREATOR_ADDR = GlobalState('CREATOR_ADDR')
+    # locals
+    ALGOS_TO_WITHDRAW = LocalState('ALGOS_TO_WITHDRAW')
+    TOKENS_TO_WITHDRAW = LocalState('TOKENS_TO_WITHDRAW')
+    USER_LIQUIDITY_TOKENS = LocalState('USER_LIQUIDITY_TOKENS')
+
     # Exchange rate decimal points
     ratio_decimal_points = Int(ratio_decimal_points)
 
-    is_creator = Txn.sender() == App.globalGet(Bytes('CREATOR'))
+    is_creator = Txn.sender() == CREATOR_ADDR.get()
 
     tx_ratio = Gtxn[1].asset_amount() * ratio_decimal_points / Gtxn[2].amount()
-
-    liquidity_calc = Gtxn[2].amount() * App.globalGet(
-        Bytes('TOTAL_LIQUIDITY_TOKENS')
-    ) / App.globalGet(Bytes('ALGOS_BALANCE'))
-
-    algos_calc = App.globalGet(Bytes('ALGOS_BALANCE')) * (
-        App.localGet(Int(0), Bytes('USER_LIQUIDITY_TOKENS')
-    ) / App.globalGet(Bytes('TOTAL_LIQUIDITY_TOKENS')))
-
-    token_calc = App.globalGet(Bytes('TOKENS_BALANCE')) * (
-        App.localGet(Int(0), Bytes('USER_LIQUIDITY_TOKENS')
-    ) / App.globalGet(Bytes('TOTAL_LIQUIDITY_TOKENS')))
+    liquidity_calc = Gtxn[2].amount() * TOTAL_LIQUIDITY_TOKENS.get() / ALGOS_BALANCE.get()
+    algos_calc = ALGOS_BALANCE.get() * USER_LIQUIDITY_TOKENS.get() / TOTAL_LIQUIDITY_TOKENS.get()
+    token_calc = TOKENS_BALANCE.get() * USER_LIQUIDITY_TOKENS.get() / TOTAL_LIQUIDITY_TOKENS.get()
 
     on_create = Seq([
-        App.globalPut(Bytes('TOKENS_BALANCE'), Int(0)),
-        App.globalPut(Bytes('ALGOS_BALANCE'), Int(0)),
-        App.globalPut(Bytes('TOTAL_LIQUIDITY_TOKENS'), Int(0)),
-        App.globalPut(Bytes('CREATOR'), Txn.sender()),
+        TOKENS_BALANCE.put(Int(0)),
+        ALGOS_BALANCE.put(Int(0)),
+        TOTAL_LIQUIDITY_TOKENS.put(Int(0)),
+        CREATOR_ADDR.put(Txn.sender()),
         Return(Int(1))
     ])
 
@@ -33,17 +37,17 @@ def state(ratio_decimal_points):
         # Update escrow address after creating it
         Assert(And(
             is_creator,
-            Txn.application_args.length() == Int(1),
+            Txn.application_args.length() == Int(2),
         )),
-        App.globalPut(Bytes('ESCROW'), Txn.application_args[0]),
+        ESCROW_ADDR.put(Txn.application_args[1]),
         Return(Int(1))
     ])
 
     on_register = Seq([
         # Set default values for user
-        App.localPut(Int(0), Bytes('TOKENS_TO_WITHDRAW'), Int(0)),
-        App.localPut(Int(0), Bytes('ALGOS_TO_WITHDRAW'), Int(0)),
-        App.localPut(Int(0), Bytes('USER_LIQUIDITY_TOKENS'), Int(0)),
+        TOKENS_TO_WITHDRAW.put(Int(0)),
+        ALGOS_TO_WITHDRAW.put(Int(0)),
+        USER_LIQUIDITY_TOKENS.put(Int(0)),
         Return(Int(1))
     ])
 
@@ -53,54 +57,30 @@ def state(ratio_decimal_points):
             Gtxn[0].type_enum() == TxnType.ApplicationCall,
             Gtxn[1].type_enum() == TxnType.AssetTransfer,
             Gtxn[2].type_enum() == TxnType.Payment,
-            Gtxn[1].asset_receiver() == App.globalGet(Bytes('ESCROW')),
-            Gtxn[2].receiver() == App.globalGet(Bytes('ESCROW')),
+            Gtxn[1].asset_receiver() == ESCROW_ADDR.get(),
+            Gtxn[2].receiver() == ESCROW_ADDR.get(),
         )),
         If(
             # Check if transactions exchange rate matches or is max 1% different from current
-            App.globalGet(Bytes('EXCHANGE_RATE')) > tx_ratio,
-            Assert(
-                App.globalGet(Bytes('EXCHANGE_RATE')) - tx_ratio
-                * ratio_decimal_points / App.globalGet(Bytes('EXCHANGE_RATE'))
-                < Int(10000)
-            ),
-            Assert(
-                tx_ratio - App.globalGet(Bytes('EXCHANGE_RATE'))
-                * ratio_decimal_points / App.globalGet(Bytes('EXCHANGE_RATE'))
-                < Int(10000)
-            )
+            EXCHANGE_RATE.get() > tx_ratio,
+            Assert(EXCHANGE_RATE.get() - tx_ratio * ratio_decimal_points / EXCHANGE_RATE.get() < Int(10000)),
+            Assert(tx_ratio - EXCHANGE_RATE.get() * ratio_decimal_points / EXCHANGE_RATE.get() < Int(10000))
         ),
         If(
             # If its first transaction then add tokens directly from txn amount, else based on calculations
-            App.globalGet(Bytes('TOTAL_LIQUIDITY_TOKENS')) == Int(0),
+            TOTAL_LIQUIDITY_TOKENS.get() == Int(0),
             Seq([
-                App.localPut(Int(0), Bytes('USER_LIQUIDITY_TOKENS'), Gtxn[2].amount()),
-                App.globalPut(Bytes('TOTAL_LIQUIDITY_TOKENS'), Gtxn[2].amount()),
+                USER_LIQUIDITY_TOKENS.put(Gtxn[2].amount()),
+                TOTAL_LIQUIDITY_TOKENS.put(Gtxn[2].amount()),
             ]),
             Seq([
-                App.localPut(
-                    Int(0),
-                    Bytes('USER_LIQUIDITY_TOKENS'),
-                    App.localGet(Int(0), Bytes('USER_LIQUIDITY_TOKENS')) + liquidity_calc
-                ),
-                App.globalPut(
-                    Bytes('TOTAL_LIQUIDITY_TOKENS'),
-                    App.globalGet(Bytes('TOTAL_LIQUIDITY_TOKENS')) + liquidity_calc
-                )
+                USER_LIQUIDITY_TOKENS.put(USER_LIQUIDITY_TOKENS.get() + liquidity_calc),
+                TOTAL_LIQUIDITY_TOKENS.put(TOTAL_LIQUIDITY_TOKENS.get() + liquidity_calc),
             ])
         ),
-        App.globalPut(
-            Bytes('TOKENS_BALANCE'),
-            App.globalGet(Bytes('TOKENS_BALANCE')) + Gtxn[1].asset_amount()
-        ),
-        App.globalPut(
-            Bytes('ALGOS_BALANCE'),
-            App.globalGet(Bytes('ALGOS_BALANCE')) + Gtxn[2].amount()
-        ),
-        App.globalPut(
-            Bytes('EXCHANGE_RATE'),
-            App.globalGet(Bytes('ALGOS_BALANCE')) * ratio_decimal_points / App.globalGet(Bytes('TOKENS_BALANCE'))
-        ),
+        TOKENS_BALANCE.put(TOKENS_BALANCE.get() + Gtxn[1].asset_amount()),
+        ALGOS_BALANCE.put(ALGOS_BALANCE.get() + Gtxn[2].amount()),
+        EXCHANGE_RATE.put(ALGOS_BALANCE.get() * ratio_decimal_points / TOKENS_BALANCE.get()),
         Return(Int(1))
     ])
 
@@ -108,18 +88,15 @@ def state(ratio_decimal_points):
         Assert(And(
             Global.group_size() == Int(1),
             Txn.application_args.length() == Int(2),
-            App.localGet(Int(0), Bytes('USER_LIQUIDITY_TOKENS')) >= Btoi(Txn.application_args[1]),
-            App.globalGet(Bytes('ALGOS_BALANCE')) > algos_calc,
-            App.globalGet(Bytes('TOKENS_BALANCE')) > token_calc,
+            USER_LIQUIDITY_TOKENS.get() >= Btoi(Txn.application_args[1]),
+            ALGOS_BALANCE.get() > algos_calc,
+            TOKENS_BALANCE.get() > token_calc,
         )),
-        App.localPut(Int(0), Bytes('ALGOS_TO_WITHDRAW'), algos_calc),
-        App.localPut(Int(0), Bytes('TOKENS_TO_WITHDRAW'), token_calc),
-        App.globalPut(Bytes('ALGOS_BALANCE'), App.globalGet(Bytes('ALGOS_BALANCE')) - algos_calc),
-        App.globalPut(Bytes('TOKENS_BALANCE'), App.globalGet(Bytes('TOKENS_BALANCE')) - token_calc),
-        App.globalPut(
-            Bytes('EXCHANGE_RATE'),
-            App.globalGet(Bytes('ALGOS_BALANCE')) * ratio_decimal_points / App.globalGet(Bytes('TOKENS_BALANCE'))
-        ),
+        ALGOS_TO_WITHDRAW.put(algos_calc),
+        TOKENS_TO_WITHDRAW.put(token_calc),
+        ALGOS_BALANCE.put(ALGOS_BALANCE.get() - algos_calc),
+        TOKENS_BALANCE.put(TOKENS_BALANCE.get() - token_calc),
+        EXCHANGE_RATE.put(ALGOS_BALANCE.get() * ratio_decimal_points / TOKENS_BALANCE.get()),
         Return(Int(1))
     ])
 
@@ -132,95 +109,68 @@ def state(ratio_decimal_points):
             [
                 Gtxn[1].type_enum() == TxnType.AssetTransfer,
                 Seq([
-                    Assert(
-                        Gtxn[1].asset_receiver() == App.globalGet(Bytes('ESCROW')),
-                    ),
-                    App.globalPut(
-                        Bytes('TOKENS_BALANCE'),
-                        App.globalGet(Bytes('TOKENS_BALANCE')) + Gtxn[1].asset_amount()
-                    ),
-                    App.localPut(
-                        Int(0),
-                        Bytes('ALGOS_TO_WITHDRAW'),
-                        (App.globalGet(Bytes('EXCHANGE_RATE'))
-                         * (Gtxn[1].asset_amount() * Int(100) / Int(103)))
+                    Assert(Gtxn[1].asset_receiver() == ESCROW_ADDR.get()),
+                    TOKENS_BALANCE.put(TOKENS_BALANCE.get() + Gtxn[1].asset_amount()),
+                    ALGOS_TO_WITHDRAW.put(
+                        (EXCHANGE_RATE.get() * (Gtxn[1].asset_amount() * Int(100) / Int(103)))
                         / ratio_decimal_points
                     ),
-                    App.globalPut(
-                        Bytes('ALGOS_BALANCE'),
-                        App.globalGet(Bytes('ALGOS_BALANCE')) - App.localGet(Int(0), Bytes('ALGOS_TO_WITHDRAW'))
-                    ),
+                    ALGOS_BALANCE.put(ALGOS_BALANCE.get() - ALGOS_TO_WITHDRAW.get()),
                 ])
             ],
             [
                 Gtxn[1].type_enum() == TxnType.Payment,
                 Seq([
-                    Assert(
-                        Gtxn[1].receiver() == App.globalGet(Bytes('ESCROW')),
-                    ),
-                    App.globalPut(
-                        Bytes('ALGOS_BALANCE'),
-                        App.globalGet(Bytes('ALGOS_BALANCE')) + Gtxn[1].amount()
-                    ),
-                    App.localPut(
-                        Int(0),
-                        Bytes('TOKENS_TO_WITHDRAW'),
+                    Assert(Gtxn[1].receiver() == ESCROW_ADDR.get()),
+                    ALGOS_BALANCE.put(ALGOS_BALANCE.get() + Gtxn[1].amount()),
+                    TOKENS_TO_WITHDRAW.put(
                         (Gtxn[1].amount() * Int(100) / Int(103))
-                        * ratio_decimal_points / App.globalGet(Bytes('EXCHANGE_RATE'))
+                        * ratio_decimal_points / EXCHANGE_RATE.get()
                     ),
-                    App.globalPut(
-                        Bytes('TOKENS_BALANCE'),
-                        App.globalGet(Bytes('TOKENS_BALANCE')) - App.localGet(Int(0), Bytes('TOKENS_TO_WITHDRAW'))
-                    ),
+                    TOKENS_BALANCE.put(TOKENS_BALANCE.get() - TOKENS_TO_WITHDRAW.get()),
                 ])
             ]
         ),
-        App.globalPut(
-            Bytes('EXCHANGE_RATE'),
-            (App.globalGet(Bytes('TOKENS_BALANCE')) * ratio_decimal_points / App.globalGet(Bytes('ALGOS_BALANCE')))
-        ),
+        EXCHANGE_RATE.put(TOKENS_BALANCE.get() * ratio_decimal_points / ALGOS_BALANCE.get()),
         Return(Int(1))
     ])
 
     on_withdraw = Seq([
-        Assert(
-            Global.group_size() == Int(2),
-        ),
-        If(
-            Gtxn[1].type_enum() == TxnType.AssetTransfer,
-            Seq([
-                Assert(And(
-                    Gtxn[1].asset_amount() == App.localGet(Int(0), Bytes('TOKENS_TO_WITHDRAW')),
-                    Gtxn[1].asset_sender() == App.globalGet(Bytes('ESCROW')),
-                )),
-                App.localPut(Int(0), Bytes('TOKENS_TO_WITHDRAW'), Int(0))
-            ]),
-            If(
+        Assert(Global.group_size() == Int(2)),
+        Cond(
+            [
+                Gtxn[1].type_enum() == TxnType.AssetTransfer,
+                Seq([
+                    Assert(And(
+                        Gtxn[1].asset_amount() == TOKENS_TO_WITHDRAW.get(),
+                        Gtxn[1].asset_sender() == ESCROW_ADDR.get(),
+                    )),
+                    TOKENS_TO_WITHDRAW.put(Int(0)),
+                ])
+            ],
+            [
                 Gtxn[1].type_enum() == TxnType.Payment,
                 Seq([
                     Assert(And(
-                        Gtxn[1].amount() == App.localGet(Int(0), Bytes('ALGOS_TO_WITHDRAW')),
-                        Gtxn[1].sender() == App.globalGet(Bytes('ESCROW')),
+                        Gtxn[1].amount() == ALGOS_TO_WITHDRAW.get(),
+                        Gtxn[1].sender() == ESCROW_ADDR.get(),
                     )),
-                    App.localPut(Int(0), Bytes('ALGOS_TO_WITHDRAW'), Int(0))
+                    ALGOS_TO_WITHDRAW.put(Int(0)),
                 ]),
-                Return(Int(0))
-            )
+            ]
         ),
         # Remove 1000 Algos that is taken as a fee
-        App.globalPut(Bytes('ALGOS_BALANCE'), App.globalGet(Bytes('ALGOS_BALANCE')) - Int(1000)),
-        App.globalPut(
-            Bytes('EXCHANGE_RATE'),
-            (App.globalGet(Bytes('TOKENS_BALANCE')) * ratio_decimal_points / App.globalGet(Bytes('ALGOS_BALANCE')))
-        ),
+        ALGOS_BALANCE.put(ALGOS_BALANCE.get() - Int(1000)),
+        EXCHANGE_RATE.put(TOKENS_BALANCE.get() * ratio_decimal_points / ALGOS_BALANCE.get()),
         Return(Int(1))
     ])
 
     return Cond(
         [Txn.application_id() == Int(0), on_create],
-        [Txn.on_completion() == OnComplete.UpdateApplication, on_update],
+        [Txn.on_completion() == OnComplete.UpdateApplication, Return(is_creator)],
         [Txn.on_completion() == OnComplete.DeleteApplication, Return(is_creator)],
         [Txn.on_completion() == OnComplete.OptIn, on_register],
+        [Txn.application_args[0] == Bytes('UPDATE'), on_update],
         [Txn.application_args[0] == Bytes('ADD_LIQUIDITY'), on_add_liquidity],
         [Txn.application_args[0] == Bytes('REMOVE_LIQUIDITY'), on_remove_liquidity],
         [Txn.application_args[0] == Bytes('SWAP'), on_swap],
@@ -229,11 +179,14 @@ def state(ratio_decimal_points):
 
 
 def clear():
+    ALGOS_TO_WITHDRAW = LocalState('ALGOS_TO_WITHDRAW')
+    TOKENS_TO_WITHDRAW = LocalState('TOKENS_TO_WITHDRAW')
+    USER_LIQUIDITY_TOKENS = LocalState('USER_LIQUIDITY_TOKENS')
     # Refuse to clear users state if he still has money to withdraw or liquidity tokens
     return And(
-        App.localGet(Int(0), Bytes('TOKENS_TO_WITHDRAW')) == Int(0),
-        App.localGet(Int(0), Bytes('ALGOS_TO_WITHDRAW')) == Int(0),
-        App.localGet(Int(0), Bytes('USER_LIQUIDITY_TOKENS')) == Int(0),
+        TOKENS_TO_WITHDRAW.get() == Int(0),
+        ALGOS_TO_WITHDRAW.get() == Int(0),
+        USER_LIQUIDITY_TOKENS.get() == Int(0),
     )
 
 
