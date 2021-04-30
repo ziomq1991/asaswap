2cbed11cdc7277634a6bba034e18403efa2de82a
Pawel Rejkowicz <pawel.rejkowicz@gmail.com>
2021-04-30 09:40:03 +0000

    adding py tests

--- contracts/assets/helpers/calc.py
+++ contracts/assets/helpers/calc.py
@@ -18,11 +18,12 @@ def make_teal(make_steps):
     return start, end
 
 class mulw_divw4(Expr):
-    def __init__(self, a, PT, A, iters):
+    def __init__(self, m1, m2, d, iters):
+        assert iters > 1
         self.outputType = TealType.uint64
-        self.a = a
-        self.PT = PT
-        self.A = A
+        self.m1 = m1
+        self.m2 = m2
+        self.d = d
         self.iters = iters
     def __teal__(self):
         def steps():
@@ -34,7 +35,7 @@ class mulw_divw4(Expr):
                 if iter:
                     yield Op.mulw,
                 else:
-                    yield Op.mulw, self.a, self.PT
+                    yield Op.mulw, self.m1, self.m2
 
                 yield Op.dup2,
                 yield Op.pop,
@@ -68,7 +69,7 @@ class mulw_divw4(Expr):
                         yield Op.load, MF
                 else:
                     yield Op.bitwise_not, Int(0)
-                    yield Op.dup, self.A
+                    yield Op.dup, self.d
                     yield Op.store, A
                     yield Op.mod,
                     yield Op.dup,
@@ -86,11 +87,11 @@ class mulw_divw4(Expr):
         return make_teal(steps)
 
     def __str__(self):
-        return "(mulw_divw {} {} {})".format(self.a, self.PT, self.A)
+        return "(mulw_divw {} {} {})".format(self.m1, self.m2, self.d)
 
     def type_of(self):
         return self.outputType
 
 
-def mulw_divw(a, PT, A):
-    return mulw_divw4(a, PT, A, 30)
+def mulw_divw(m1, m2, d):
+    return mulw_divw4(m1, m2, d, 30)
